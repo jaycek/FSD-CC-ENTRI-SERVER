6f858efab2a28ff98f81b4f7dfd31190a67d58d2
Jayce <ashishalex084@gmail.com>
2024-04-29 18:05:02 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors'
+import userRouter from './Routers/userRouter.js'
+import employeeRouter from './Routers/employeeRouter.js'
+import postRouter from './Routers/postRouter.js'
+import path from 'path'
+import 'dotenv/config';
+import mongoose from "mongoose";
+
+async function main() {
+    
+    await mongoose.connect(process.env.MONGODB_URL);
+}           
+main().then(console.log("DB connected")).catch(err => console.log(err));
+
+
+const app=express()
+
+app.use(express.json())
+app.use(cors())
+app.use('/users',userRouter)
+app.use('/employees',employeeRouter)
+app.use('/posts',postRouter)
+
+const __dirname = path.resolve();
+// Serve static files from the 'public' folder
+
+app.use(express.static(path.join(__dirname, 'uploads')));
+
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`)
+})
+
